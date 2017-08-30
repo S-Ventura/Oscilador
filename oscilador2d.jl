03907aaa582edbb38dc5280c9df3f2b90cbb3879
Hector <cote@ciencias.unam.mx>
2017-08-29 20:56:31 -0500

    Primeros ejercicios con osciladores

---
+++ oscilador2d.jl
@@ -0,0 +1,48 @@
+using PyPlot
+
+# Constantes
+
+m=1;
+
+#k1=1.0; #constante del resorte en x
+#k2=1.0; #constante del resorte en y
+
+#w1=sqrt(k1/m);
+#w2=sqrt(k2/m);
+
+w1=3
+w2=4
+
+delta=pi/2;
+alfa=pi/2;
+
+A=1;
+B=1;
+
+#Arreglos
+
+t = linspace(0, 2*pi, 1000); #periodo de  0 a 2pi
+
+x=[]
+y=[]
+
+for i=1:length(t)
+    valx=A*cos(w1*t[i]+delta)
+    x=push!(x,valx)
+
+    valy=B*cos(w2*t[i]+alfa)
+    y=push!(y,valy)
+end
+
+#Gráfica
+
+title("Oscilador en 2D")#Título de la gráfica
+xlabel("\$x\$") #Etiqueta para el eje X
+ylabel("\$y\$")#Etiqueta para el eje Y
+
+axhline(0, color="red")
+grid() #malla
+
+plot(x,y)
+
+show()
